b351de5033a4c8913552d1a191e98758fa729ca9
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-04 11:23:30 +0500

    Defines route for category news

--- app/routes/geonews.route.js
+++ app/routes/geonews.route.js
@@ -3,6 +3,7 @@ const geonews = require("../controllers/geonews.controller");
 
 module.exports = (app) => {
   router.get("/", geonews.get);
+  router.get("/:category", geonews.getByCategory);
 
   app.use("/api/geonews", router);
 };
