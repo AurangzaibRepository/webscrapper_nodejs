858dbae2ba740788be4c700dfe1fd73f42f00203
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-04 13:06:48 +0500

    Implements route for tehenws

--- app/routes/thenews.route.js
+++ app/routes/thenews.route.js
@@ -1,2 +1,8 @@
 const router = require("express").Router();
 const thenews = require("../controllers/thenews.controller");
+
+module.exports = (app) => {
+  router.get("/", thenews.get);
+
+  app.use("/api/thenews", router);
+};
