6cd6b2310ccd2c38aba658a7f6571dfea3f5bec9
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-04 12:48:03 +0500

    Refactrs parser function

--- app/parsers/geonews.parser.js
+++ app/parsers/geonews.parser.js
@@ -17,11 +17,41 @@ function parseContents(selector, data) {
   return contents;
 }
 
-exports.parse = (data) => {
+function parseCategoryContens(data) {
+  const $ = cheerio.load(data);
   const contents = [];
 
-  contents.push(parseContents(".m_c_left ul li", data));
-  contents.push(parseContents(".m_c_right ul li", data));
+  const baseElement = $(".FirstBlock");
+  contents.push({
+    title: baseElement.find(".entry-title a").attr("title").trim(),
+    url: baseElement.find(".entry-title a").attr("href"),
+    image: baseElement.find(".pic img").attr("src"),
+  });
+
+  $(".singleBlock", data).each((i, element) => {
+    const listItem = $(element).find("ul li");
+
+    contents.push({
+      title: listItem.find("a").attr("title").trim(),
+      url: listItem.find("a").attr("href"),
+      image: $(element).find(".pic img").attr("src"),
+    });
+  });
+
+  return contents;
+}
+
+exports.parse = (data) => {
+  let contents = [];
+
+  contents = parseContents(".m_c_left ul li", data);
+  contents = contents.concat(parseContents(".m_c_right ul li", data));
+
+  return contents;
+};
+
+exports.parseByCategory = (data) => {
+  const contents = parseCategoryContens(data);
 
   return contents;
 };
