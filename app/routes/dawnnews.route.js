363f1ff53a00ad9e1dfc423cbb23512225acc787
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-04 20:47:40 +0500

    Implements route

--- app/routes/dawnnews.route.js
+++ app/routes/dawnnews.route.js
@@ -1,2 +1,8 @@
 const router = require("express").Router();
 const dawnnews = require("../controllers/dawnnews.controller");
+
+module.exports = (app) => {
+  router.get("/", dawnnews.get);
+
+  app.use("/api/dawnnews", router);
+};
