513181c15d3fe798f5e5990ec7db696634f53881
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-04 13:03:35 +0500

    Includes route for thenews

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 require("./app/routes/skynews.route")(app);
 require("./app/routes/arynews.route")(app);
 require("./app/routes/geonews.route")(app);
+require("./app/routes/thenews.route")(app);
 
 const PORT = process.env.port || 8000;
 app.listen(PORT, () => {
