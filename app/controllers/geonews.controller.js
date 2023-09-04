5467fe967703afd1edfdd9794af47ffc51b14da1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-04 11:17:37 +0500

    Adds res.send

--- app/controllers/geonews.controller.js
+++ app/controllers/geonews.controller.js
@@ -4,16 +4,16 @@ const requestHelper = require("../utils/request.helper");
 exports.get = (req, res) => {
   geonewService.extractData()
     .then((response) => {
-      requestHelper.getResponse(
+      res.send(requestHelper.getResponse(
         true,
         null,
         response,
-      );
+      ));
     })
     .catch((error) => {
-      requestHelper.getResponse(
+      res.send(requestHelper.getResponse(
         false,
-        error.message,
-      );
+        error,
+      ));
     });
 };
