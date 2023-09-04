0dc3a33c896a426d82faad562e54a8a4e5164332
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-04 09:21:53 +0500

    Includes roter file for geo news

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 require("dotenv").config();
 require("./app/routes/skynews.route")(app);
 require("./app/routes/arynews.route")(app);
+require("./app/routes/geonews.route")(app);
 
 const PORT = process.env.port || 8000;
 app.listen(PORT, () => {
