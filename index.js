b3521d6e78a85b25683fd18580e3f98333838327
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-04 20:31:18 +0500

    Includes route file for dawnnews

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("./app/routes/skynews.route")(app);
 require("./app/routes/arynews.route")(app);
 require("./app/routes/geonews.route")(app);
 require("./app/routes/thenews.route")(app);
+require("./app/routes/dawnnews.route")(app);
 
 const PORT = process.env.port || 8000;
 app.listen(PORT, () => {
