93eded48afbad39f2a881901b219e53bb5913649
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-04 09:45:53 +0500

    Implements extract dat fnction

--- app/services/geonews.service.js
+++ app/services/geonews.service.js
@@ -1,2 +1,17 @@
 const axios = require("Axios");
 const parser = require("../parsers/geonews.parser");
+
+exports.extractData = () => {
+  const promise = new Promise((resolve, reject) => {
+    axios(process.env.GEONEWS_URL)
+      .then((response) => {
+        const contents = parser.parse(response);
+        resolve(contents);
+      })
+      .catch((error) => {
+        reject(error.message);
+      });
+  });
+
+  return promise;
+};
