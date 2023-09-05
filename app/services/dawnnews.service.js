3da9079c46df3f1509d58d0cf46fe000182fa086
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-05 17:12:50 +0500

    Implements service function o get data

--- app/services/dawnnews.service.js
+++ app/services/dawnnews.service.js
@@ -1,2 +1,17 @@
 const axios = require("axios");
 const parser = require("../parsers/dawnnews.parser");
+
+exports.extractData = () => {
+  const promise = new Promise((resolve, reject) => {
+    axios(process.env.DAWNNEWS_URL)
+      .then((response) => {
+        const contents = parser.parse(response.data);
+        resolve(contents);
+      })
+      .catch((error) => {
+        reject(error.message);
+      });
+  });
+
+  return promise;
+};
