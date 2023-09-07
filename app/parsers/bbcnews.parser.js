4ecbe6ea5d29bb177c915f23ec89a23b65acd7c6
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-07 21:58:52 +0500

    Implements functin to parse url and tags

--- app/parsers/bbcnews.parser.js
+++ app/parsers/bbcnews.parser.js
@@ -1,18 +1,45 @@
 const cheerio = require("cheerio");
 
-exports.parse = (data) => {
+function parseUrl(url, section) {
+  const revisedUrl = (section != null ? `${process.env.BBCNEWS_URL}${url}` : url);
+
+  return revisedUrl;
+}
+
+function createTags(name, url) {
+  return {
+    name,
+    url: `${process.env.BBCNEWS_URL}${url}`,
+  };
+}
+
+function parseSectionContents(data, rootElement, section = null) {
   const $ = cheerio.load(data);
   const contents = [];
 
-  $(".module--promo .media-list li", data).each((index, element) => {
-    const headerElement = $(".media__title a");
+  $(`${rootElement} .media-list li`, data).each((index, element) => {
+    const headerElement = $(element).find(".media__title .media__link");
 
     contents.push({
       title: headerElement.text().trim(),
-      url: headerElement.attr("href"),
-      image: $(element).find(".media__image img").attr("src"),
+      summary: $(element).find(".media__summary").text().trim(),
+      url: parseUrl(headerElement.attr("href"), section),
+      tagInfo: createTags(
+        $(element).find(".media__tag").text().trim(),
+        $(element).find(".media__tag").attr("href")
+      ),
     });
   });
+
+  return contents;
+}
+
+exports.parse = (data) => {
+  const contents = {
+    promo: parseSectionContents(data, ".module--promo"),
+    news: parseSectionContents(data, ".module--news", "news"),
+    sports: parseSectionContents(data, ".module--sport", "sport"),
+  };
 
   return contents;
 };
