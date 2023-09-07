4a936024298ab27b133e89a9cf176669c2af84b4
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-07 17:48:39 +0500

    Implements controller get action

--- app/controllers/bbcnews.controller.js
+++ app/controllers/bbcnews.controller.js
@@ -1,2 +1,19 @@
 const bbcnewsService = require("../services/bbcnews.service");
 const requestHelper = require("../utils/request.helper");
+
+exports.get = (req, res) => {
+  bbcnewsService.extractData()
+    .then((response) => {
+      res.send(requestHelper.getResponse(
+        true,
+        null,
+        response,
+      ));
+    })
+    .catch((error) => {
+      res.send(requestHelper.getResponse(
+        false,
+        error,
+      ));
+    });
+};
