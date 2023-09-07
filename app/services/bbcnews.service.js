4f1175e6b525330c896ba61f83706f31a4c64398
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-07 22:47:43 +0500

    Amends parser file path

--- app/services/bbcnews.service.js
+++ app/services/bbcnews.service.js
@@ -1,11 +1,27 @@
 const axios = require("axios");
-const parser = require("../parsers/bbcnews.parser");
+const parser = require("../parsers/bbc/home.parser");
+const categoryParser = require("../parsers/bbc/category.parser");
 
 exports.extractData = () => {
   const promise = new Promise((resolve, reject) => {
     axios(process.env.BBCNEWS_URL)
       .then((response) => {
         const contents = parser.parse(response.data);
+        resolve(contents);
+      })
+      .catch((error) => {
+        reject(error.message);
+      });
+  });
+
+  return promise;
+};
+
+exports.extractCategoryData = (category) => {
+  const promise = new Promise((resolve, reject) => {
+    axios(`${process.env.BBCNEWS_URL}/news/${category}`)
+      .then((response) => {
+        const contents = categoryParser.parse(response);
         resolve(contents);
       })
       .catch((error) => {
