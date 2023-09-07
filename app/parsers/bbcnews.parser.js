125f9a168f179c7ba066e256f14f8141334afead
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-07 19:57:04 +0500

    Implemented parser function for bbc

--- app/parsers/bbcnews.parser.js
+++ app/parsers/bbcnews.parser.js
@@ -1,5 +1,18 @@
 const cheerio = require("cheerio");
 
 exports.parse = (data) => {
+  const $ = cheerio.load(data);
+  const contents = [];
 
+  $(".module--promo .media-list li", data).each((index, element) => {
+    const headerElement = $(".media__title a");
+
+    contents.push({
+      title: headerElement.text().trim(),
+      url: headerElement.attr("href"),
+      image: $(element).find(".media__image img").attr("src"),
+    });
+  });
+
+  return contents;
 };
