53235ec1a160369a112a356d3958a862b6e310a0
Auranzgaib Najam <aurangzaib_najam@hotmail.com>
2023-09-08 18:29:01 +0500

    Implements service function

--- app/services/thestarnews.service.js
+++ app/services/thestarnews.service.js
@@ -1,2 +1,17 @@
 const axios = require("axios");
 const parser = require("../parsers/thestarnews.parser");
+
+exports.extractData = () => {
+  const promise = new Promise((resolve, reject) => {
+    axios(process.env.THESTARNEWS_URL)
+      .then((response) => {
+        const contents = parser.parse(response.data);
+        resolve(contents);
+      })
+      .catch((error) => {
+        reject(error.message);
+      });
+  });
+
+  return promise;
+};
