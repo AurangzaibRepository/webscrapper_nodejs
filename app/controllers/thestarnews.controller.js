354ca06894980fdc7ff9d4e5aadc95aa7604e0d6
Auranzgaib Najam <aurangzaib_najam@hotmail.com>
2023-09-08 18:08:30 +0500

    Implements controller function

--- app/controllers/thestarnews.controller.js
+++ app/controllers/thestarnews.controller.js
@@ -1,2 +1,17 @@
 const thestarnewsService = require("../services/thestarnews.service");
 const requestHelper = require("../utils/request.helper");
+
+exports.get = (req, res) => {
+  thestarnewsService.extractData()
+    .then((response) => {
+      true,
+      null,
+      response,
+    })
+    .catch((error) => {
+      res.send(requestHelper.getResponse(
+        false,
+        error,
+      ));
+    });
+};
