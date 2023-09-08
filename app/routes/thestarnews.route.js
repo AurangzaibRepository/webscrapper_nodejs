ae60680c9e75227734e27397231f7b9117ae75c4
Auranzgaib Najam <aurangzaib_najam@hotmail.com>
2023-09-08 17:57:06 +0500

    Defines route for thestarnews

--- app/routes/thestarnews.route.js
+++ app/routes/thestarnews.route.js
@@ -1,2 +1,8 @@
 const router = require("express").Router();
 const thestarnews = require("../controllers/thestarnews.controller");
+
+module.exports = (app) => {
+  router.get("/", thestarnews.get);
+
+  app.use("/api/thestarnews", router);
+};
