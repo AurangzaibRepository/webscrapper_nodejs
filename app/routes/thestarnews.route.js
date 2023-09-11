a2f50875662b9cfd12856d29936d3e98863a7131
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-11 17:40:47 +0500

    Defines route for category

--- app/routes/thestarnews.route.js
+++ app/routes/thestarnews.route.js
@@ -3,6 +3,7 @@ const thestarnews = require("../controllers/thestarnews.controller");
 
 module.exports = (app) => {
   router.get("/", thestarnews.get);
+  router.get("/:category", thestarnews.getByCategory);
 
   app.use("/api/thestarnews", router);
 };
