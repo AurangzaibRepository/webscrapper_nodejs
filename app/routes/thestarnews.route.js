7314b3869ef0b444439684f5e5595f2da3d9b9ce
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-11 17:44:12 +0500

    Specifies optional parameter

--- app/routes/thestarnews.route.js
+++ app/routes/thestarnews.route.js
@@ -2,8 +2,7 @@ const router = require("express").Router();
 const thestarnews = require("../controllers/thestarnews.controller");
 
 module.exports = (app) => {
-  router.get("/", thestarnews.get);
-  router.get("/:category", thestarnews.getByCategory);
+  router.get("/:category?", thestarnews.get);
 
   app.use("/api/thestarnews", router);
 };
