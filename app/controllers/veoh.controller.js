d1736c7ec8cb3c5c9db4abe1b135f0b38062dc53
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-12 19:42:13 +0500

    Implements controller action

--- app/controllers/veoh.controller.js
+++ app/controllers/veoh.controller.js
@@ -1,2 +1,19 @@
 const veohService = require("../services/veoh.service");
 const requestHelper = require("../utils/request.helper");
+
+exports.get = (req, res) => {
+  veohService.extractData()
+    .then((response) => {
+      res.send(requestHelper.getResponse(
+        true,
+        null,
+        response,
+      ));
+    })
+    .catch((error) => {
+      res.send(requestHelper.getResponse(
+        false,
+        error,
+      ));
+    });
+};
