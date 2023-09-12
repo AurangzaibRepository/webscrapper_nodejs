fb870f7026163e20bb2301f0c028d92f012d5518
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-12 12:30:10 +0500

    Implemens controller action

--- app/controllers/youtube.controller.js
+++ app/controllers/youtube.controller.js
@@ -1,2 +1,19 @@
-const youtubeService = require("../services/youtube .service");
+const youtubeService = require("../services/youtube.service");
 const requestHelper = require("../utils/request.helper");
+
+exports.get = (req, res) => {
+  youtubeService.extractData(req.params.category)
+    .then((response) => {
+      res.send(requestHelper.getResponse(
+        true,
+        null,
+        response,
+      ));
+    })
+    .catch((error) => {
+      res.send(requestHelper.getResponse(
+        false,
+        error,
+      ));
+    });
+};
