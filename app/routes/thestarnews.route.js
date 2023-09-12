1b86eb0505d21641151a94a6e8b0a4f5896508fe
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-12 11:53:48 +0500

    Defines rouet for category

--- app/routes/thestarnews.route.js
+++ app/routes/thestarnews.route.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 const thestarnews = require("../controllers/thestarnews.controller");
 
 module.exports = (app) => {
-  router.get("/:category?", thestarnews.get);
+  router.get("/", thestarnews.get);
+  router.get("/:category", thestarnews.getByCategory);
 
   app.use("/api/thestarnews", router);
 };
