22ced23517f1bf5127a6557cd0a5b7490017534d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-12 20:45:21 +0500

    Calls parser function base don url

--- app/services/veoh.service.js
+++ app/services/veoh.service.js
@@ -2,12 +2,20 @@
 const puppeteer = require("puppeteer");
 const parser = require("../parsers/veoh/home.parser");
 
-exports.extractData = async () => {
+exports.extractData = async (keyword) => {
+  let url = process.env.VEOH_URL;
+  if (keyword != null) {
+    url += `/find/${keyword}`;
+  }
+
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto(process.env.VEOH_URL);
+  await page.goto(url);
 
-  const contents = await parser.parse(page);
+  const contents = (keyword == null
+    ? await parser.parse(page)
+    : await parser.parseByCategory(page)
+  );
 
   await browser.close();
   return Promise.resolve(contents);
