f68a310dfe5ca11bb822ed322b97bc51d203883a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-12 19:25:41 +0500

    Defines route for veih

--- app/routes/veoh.route.js
+++ app/routes/veoh.route.js
@@ -1,2 +1,8 @@
 const router = require("express").Router();
 const veoh = require("../controllers/veoh.controller");
+
+module.exports = (app) => {
+  router.get("/", veoh.get);
+
+  app.use("/api/veoh", router);
+};
