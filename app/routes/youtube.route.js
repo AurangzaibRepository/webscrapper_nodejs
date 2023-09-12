15bd8a5c04a238f91b474d890ec9837b9455701b
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-12 12:23:00 +0500

    Defines route for yoytube

--- app/routes/youtube.route.js
+++ app/routes/youtube.route.js
@@ -1,2 +1,8 @@
 const router = require("express").Router();
 const youtube = require("../controllers/youtube.controller");
+
+module.exports = (app) => {
+  router.get("/:keyword", youtube.get);
+
+  app.use("/api/youtube", router);
+};
