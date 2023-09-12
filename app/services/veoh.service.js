f0f6b3c21dab3fbad853e07b5ff4dc342038d42d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-12 20:27:29 +0500

    Calls parser function to parse html

--- app/services/veoh.service.js
+++ app/services/veoh.service.js
@@ -1,1 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const puppeteer = require("puppeteer");
+const parser = require("../parsers/veoh/home.parser");
+
+exports.extractData = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(process.env.VEOH_URL);
+
+  const contents = await parser.parse(page);
+
+  await browser.close();
+  return Promise.resolve(contents);
+};
