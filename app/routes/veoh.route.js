ca0c9eed09e678dc63e94c842ff92574dc764a8a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-12 20:52:36 +0500

    Amends route parameer anem

--- app/routes/veoh.route.js
+++ app/routes/veoh.route.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const veoh = require("../controllers/veoh.controller");
 
 module.exports = (app) => {
-  router.get("/:category?", veoh.get);
+  router.get("/:keyword?", veoh.get);
 
   app.use("/api/veoh", router);
 };
