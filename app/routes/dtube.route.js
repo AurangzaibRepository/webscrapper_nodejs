1edb273f5dc171535177d44d6106df855605b63f
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-12 22:06:50 +0500

    Defines rouet for dtube

--- app/routes/dtube.route.js
+++ app/routes/dtube.route.js
@@ -1,2 +1,8 @@
 const router = require("express").Router();
 const dtube = require("../controllers/dtube.controller");
+
+module.exports = (app) => {
+  router.get("/", dtube.get);
+
+  app.use("/api/dtube", router);
+};
