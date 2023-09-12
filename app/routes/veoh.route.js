b5cd01305d351bafc28706c70525b5c5a3e17cee
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-12 20:37:58 +0500

    Specifies optional parameter

--- app/routes/veoh.route.js
+++ app/routes/veoh.route.js
@@ -2,8 +2,7 @@ const router = require("express").Router();
 const veoh = require("../controllers/veoh.controller");
 
 module.exports = (app) => {
-  router.get("/", veoh.get);
-  router.get("/:keyword", veoh.getByCategory);
+  router.get("/:category?", veoh.get);
 
   app.use("/api/veoh", router);
 };
