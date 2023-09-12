0b924682a58df36e5c9a40960d884eb4a7c558f3
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-12 20:26:58 +0500

    Implements parser function

--- app/parsers/veoh/home.parser.js
+++ app/parsers/veoh/home.parser.js
@@ -1,3 +1,22 @@
-exports.parse = (page) => {
+exports.parse = async (page) => {
+  const contents = await page.evaluate(() => {
+    const data = [];
+    const videoContainers = document.querySelectorAll(".video_area");
 
+    videoContainers.forEach((container) => {
+      const items = container.querySelectorAll(".video_list ul li");
+
+      items.forEach((item) => {
+        data.push({
+          title: item.querySelector(".title").innerText,
+          url: item.querySelector("a").getAttribute("href"),
+          image: item.querySelector("img").getAttribute("src"),
+        });
+      });
+    });
+
+    return data;
+  });
+
+  return contents;
 };
