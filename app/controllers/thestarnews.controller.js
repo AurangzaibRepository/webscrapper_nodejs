fd69ce43eb8aea4a4420d82fbfb6e423b71d98d6
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-12 11:51:17 +0500

    Calls service function to retrieve data for category

--- app/controllers/thestarnews.controller.js
+++ app/controllers/thestarnews.controller.js
@@ -2,7 +2,24 @@ const thestarnewsService = require("../services/thestarnews.service");
 const requestHelper = require("../utils/request.helper");
 
 exports.get = (req, res) => {
-  thestarnewsService.extractData(req.params.category)
+  thestarnewsService.extractData()
+    .then((response) => {
+      res.send(requestHelper.getResponse(
+        true,
+        null,
+        response,
+      ));
+    })
+    .catch((error) => {
+      res.send(requestHelper.getResponse(
+        false,
+        error,
+      ));
+    });
+};
+
+exports.getByCategory = (req, res) => {
+  thestarnewsService.extractCategoryData(req.params.category)
     .then((response) => {
       res.send(requestHelper.getResponse(
         true,
