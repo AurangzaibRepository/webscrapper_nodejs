dd655f6dd760bac2b25213c397715dcc9d7263ed
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-13 16:59:02 +0500

    Implements srvice function

--- app/services/dtube.service.js
+++ app/services/dtube.service.js
@@ -1,1 +1,15 @@
 const puppeteer = require("puppeteer");
+const parser = require("../parsers/dtube.parser");
+
+exports.extractData = async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(process.env.DTUBE_URL, {
+    waitUntil: "networkidle0",
+  });
+
+  const contents = await parser.parse(page);
+
+  await browser.close();
+  return Promise.resolve(contents);
+};
