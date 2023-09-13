a429cce5ea5f2718cc52602d41d3e483fd608afb
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-13 21:01:39 +0500

    Passes keyword paremetr

--- app/controllers/youtube.controller.js
+++ app/controllers/youtube.controller.js
@@ -2,7 +2,7 @@ const youtubeService = require("../services/youtube.service");
 const requestHelper = require("../utils/request.helper");
 
 exports.get = (req, res) => {
-  youtubeService.extractData()
+  youtubeService.extractData(req.params.keyword)
     .then((response) => {
       res.send(requestHelper.getResponse(
         true,
