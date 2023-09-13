5b0f715a7933cc32674a64bfd16ae8c1b25546f2
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-13 19:58:29 +0500

    Implements youtube controller action

--- app/controllers/youtube.controller.js
+++ app/controllers/youtube.controller.js
@@ -1,2 +1,19 @@
 const youtubeService = require("../services/youtube.service");
 const requestHelper = require("../utils/request.helper");
+
+exports.get = (req, res) => {
+  youtubeService.extractData()
+    .then((response) => {
+      res.send(requestHelper.getResponse(
+        true,
+        null,
+        response,
+      ));
+    })
+    .catch((error) => {
+      res.send(requestHelper.getResponse(
+        false,
+        error,
+      ));
+    });
+};
