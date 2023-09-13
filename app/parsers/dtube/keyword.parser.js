38cfcba7a41affdc565d3f1c84c4da671aeadfd1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-13 18:49:19 +0500

    Implements parser for keword

--- app/parsers/dtube/keyword.parser.js
+++ app/parsers/dtube/keyword.parser.js
@@ -1,7 +1,32 @@
 exports.parse = async (page) => {
-  const contents = await page.evaluate(() => {
+  const { DTUBE_URL } = process.env;
 
-  });
+  const contents = await page.evaluate((baseUrl) => {
+    function getTagInformation(name, url) {
+      return {
+        name,
+        url: `${baseUrl}${url}`,
+      };
+    }
+
+    const data = [];
+    const items = document.querySelectorAll(".maingrid .container a[title]");
+
+    items.forEach((item) => {
+      data.push({
+        title: item.getAttribute("title").trim(),
+        description: item.querySelector(".videosnapdescription").innerText.trim(),
+        url: `${baseUrl}${item.getAttribute("href")}`,
+        image: item.querySelector(".verticalvideosnapsnap img").getAttribute("src"),
+        tagInfo: getTagInformation(
+          item.querySelector(".videosnapauthor a span").innerText,
+          item.querySelector(".videosnapauthor a").getAttribute("href"),
+        ),
+      });
+    });
+
+    return data;
+  }, DTUBE_URL);
 
   return contents;
 };
