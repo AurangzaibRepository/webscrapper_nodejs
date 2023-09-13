3b6080213e195869b51c4ad62c1da656fac0a86b
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-13 15:26:19 +0500

    Implements get function of controller

--- app/controllers/dtube.controller.js
+++ app/controllers/dtube.controller.js
@@ -1,2 +1,19 @@
 const dtubeService = require("../services/dtube.service");
 const requestHelper = require("../utils/request.helper");
+
+exports.get = (req, res) => {
+  dtubeService.extractData
+    .then((response) => {
+      res.send(requestHelper.getResponse(
+        true,
+        null,
+        response,
+      ));
+    })
+    .catch((error) => {
+      res.send(requestHelper.getResponse(
+        false,
+        error,
+      ));
+    });
+};
