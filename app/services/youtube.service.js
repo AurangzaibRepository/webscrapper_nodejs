969ca8429a75db059303b81900d799367ceee258
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-13 20:44:45 +0500

    Implements service function

--- app/services/youtube.service.js
+++ app/services/youtube.service.js
@@ -1,1 +1,20 @@
 const puppeteer = require("puppeteer");
+const parser = require("../parsers/youtube.parser");
+
+exports.extractData = async () => {
+  try {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto(process.env.YOUTUBE_URL, {
+      waitUntil: "networkidle0",
+      timeout: 0,
+    });
+
+    const contents = await parser.parse(page);
+
+    await browser.close();
+    return Promise.resolve(contents);
+  } catch (exception) {
+    return Promise.reject(exception.message);
+  }
+};
