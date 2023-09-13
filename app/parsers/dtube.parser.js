d3c4091888e996f9f109d08bb08ba57eb13f7a58
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-13 17:08:48 +0500

    Implements parser funciton

--- app/parsers/dtube.parser.js
+++ app/parsers/dtube.parser.js
@@ -1,3 +1,35 @@
 exports.parse = async (page) => {
+  const contents = await page.evaluate(() => {
+    function getTagInformation(name, url) {
+      return {
+        name,
+        url: `${process.env.DTUBE_URL}/${url}`,
+      };
+    }
 
+    const data = [];
+    const containers = document.querySelectorAll(".article .owl-carousel");
+
+    containers.forEach((container) => {
+      const items = container.querySelectorAll(".videosnap");
+
+      items.forEach((item) => {
+        const headerElement = item.querySelector(".videosnap a");
+
+        data.push({
+          title: headerElement.getAttribute("title"),
+          url: `${process.env.DTUBE_URL}/${headerElement.getAttribute("href")}`,
+          image: headerElement.querySelector("#snapimg").getAttribute("src"),
+          tagInfo: getTagInformation(
+            item.querySelector(".videosnapauthor a .customlink").innerText,
+            item.querySelector(".videosnapauthor a").getAttribute("href"),
+          ),
+        });
+      });
+    });
+
+    return data;
+  });
+
+  return contents;
 };
