03df3d0c8e2ba9b91177b58023b419e345f8d027
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-14 12:20:10 +0500

    DEfines route for dailymotopn

--- app/routes/dailymotion.route.js
+++ app/routes/dailymotion.route.js
@@ -1,2 +1,8 @@
 const router = require("express").Router();
 const dailymotion = require("../controllers/dailymotion.controller");
+
+module.exports = (app) => {
+  router.get("/", dailymotion.get);
+
+  app.use("/api/dailymotion", router);
+};
