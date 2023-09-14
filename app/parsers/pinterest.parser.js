68a19ca56cb4976a4df5e956f0c2b2c9fd78a799
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-14 18:40:31 +0500

    Passes babse url in aprser

--- app/parsers/pinterest.parser.js
+++ app/parsers/pinterest.parser.js
@@ -1,3 +1,24 @@
 exports.parse = async (page) => {
+  const { PINTEREST_URL } = process.env;
 
+  const contents = await page.evaluate((baseUrl) => {
+    const data = [];
+    const items = document.querySelectorAll(".vbI.XiG .MIw.Hb7");
+
+    items.forEach((item) => {
+      const [element] = item.querySelectorAll(".CCY.S9z");
+      const [title, caption] = element.innerText.split("\n");
+
+      data.push({
+        title,
+        caption,
+        url: `${baseUrl}${item.querySelector("a.Wk9.CCY").getAttribute("href")}`,
+        image: item.querySelector("img.hCL.kVc").getAttribute("src"),
+      });
+    });
+
+    return data;
+  }, PINTEREST_URL);
+
+  return contents;
 };
