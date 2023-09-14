c1d1fc185940ce9b31aaffb2015edf9b9513852d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-14 10:04:33 +0500

    DEfines route for channel

--- app/routes/youtube.route.js
+++ app/routes/youtube.route.js
@@ -3,6 +3,7 @@ const youtube = require("../controllers/youtube.controller");
 
 module.exports = (app) => {
   router.get("/:keyword?", youtube.get);
+  router.get("/channel/:channel", youtube.getByChannel);
 
   app.use("/api/youtube", router);
 };
