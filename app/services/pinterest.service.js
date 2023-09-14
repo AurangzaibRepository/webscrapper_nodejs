5b2df0ded7176ccd374231f74b92757ad6bf97aa
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-14 18:58:12 +0500

    Includes news parser

--- app/services/pinterest.service.js
+++ app/services/pinterest.service.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer");
-const parser = require("../parsers/pinterest.parser");
+const parser = require("../parsers/pinterest/home.parser");
+const newsParser = require("../parsers/pinterest/news.parser");
 
 exports.extractData = async (keyword) => {
   try {
@@ -19,4 +20,18 @@ exports.extractData = async (keyword) => {
   } catch (exception) {
     return Promise.reject(exception.message);
   }
+};
+
+exports.extractNews = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(process.env.PINTERESTNEWS_URL, {
+    waitUntil: "networkidle0",
+    timeout: 0,
+  });
+
+  const contents = await newsParser.parse(page);
+
+  await browser.close();
+  return Promise.resolve(contents);
 };
