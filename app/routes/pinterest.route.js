c94e6d9f23960578bfebd35abcd64b596180e8fe
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-14 19:06:31 +0500

    Moves news route t otop

--- app/routes/pinterest.route.js
+++ app/routes/pinterest.route.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const pinterest = require("../controllers/pinterest.controller");
 
 module.exports = (app) => {
-  router.get("/:keyword", pinterest.get);
   router.get("/news", pinterest.getNews);
+  router.get("/:keyword", pinterest.get);
 
   app.use("/api/pinterest", router);
 };
