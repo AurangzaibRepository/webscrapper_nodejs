666973dc307fdebe68b537667b91e40a5aa2ffd5
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-14 16:30:05 +0500

    Implements controller action

--- app/controllers/pinterest.controller.js
+++ app/controllers/pinterest.controller.js
@@ -1,2 +1,19 @@
 const pinterestService = require("../services/pinterest.service");
 const requestHelper = require("../utils/request.helper");
+
+exports.get = (req, res) => {
+  pinterestService.extractData()
+    .then((response) => {
+      res.send(requestHelper.getResponse(
+        true,
+        null,
+        response,
+      ));
+    })
+    .catch((error) => {
+      res.send(requestHelper.getResponse(
+        false,
+        error,
+      ));
+    });
+};
