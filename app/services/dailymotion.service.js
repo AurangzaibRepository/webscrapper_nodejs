2fc41fd2b0d9b6021649eebe0bbae306c9265f48
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-14 15:00:57 +0500

    Implements service function

--- app/services/dailymotion.service.js
+++ app/services/dailymotion.service.js
@@ -1,5 +1,16 @@
 const puppeteer = require("puppeteer");
+const parser = require("../parsers/dailymotion.parser");
 
 exports.extractData = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(process.env.DAILYMOTION_URL, {
+    waitUntil: "networkidle0",
+    timeout: 0,
+  });
 
+  const contents = await parser.parse(page);
+
+  await browser.close();
+  return Promise.resolve(contents);
 };
