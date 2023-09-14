190ba98895e7ebfdf38b22a77754de2db252d482
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-14 18:36:55 +0500

    Appends url path

--- app/services/pinterest.service.js
+++ app/services/pinterest.service.js
@@ -2,17 +2,21 @@ const puppeteer = require("puppeteer");
 const parser = require("../parsers/pinterest.parser");
 
 exports.extractData = async (keyword) => {
-  const url = `${process.env.PINTEREST_URL}/?q=${keyword}`;
+  try {
+    const url = `${process.env.PINTEREST_URL}/search/pins/?q=${keyword}`;
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(url, {
-    waitUntil: "networkidle0",
-    timeout: 0,
-  });
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, {
+      waitUntil: "networkidle0",
+      timeout: 0,
+    });
 
-  const contents = await parser.parse(page);
+    const contents = await parser.parse(page);
 
-  await browser.close();
-  return Promise.resolve(contents);
+    await browser.close();
+    return Promise.resolve(contents);
+  } catch (exception) {
+    return Promise.reject(exception.message);
+  }
 };
