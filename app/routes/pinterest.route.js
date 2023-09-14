27badcbce15b4873f5cea5341c979eec75e9bf66
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-14 16:21:37 +0500

    Define route for pinterest

--- app/routes/pinterest.route.js
+++ app/routes/pinterest.route.js
@@ -1,2 +1,8 @@
 const router = require("express").Router();
 const pinterest = require("../controllers/pinterest.controller");
+
+module.exports = (app) => {
+  router.get("/:keyword", pinterest.get);
+
+  app.use("/api/pinterest", router);
+};
