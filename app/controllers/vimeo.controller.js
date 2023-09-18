fbd17ed46681bf68e3ecbfdb5cd0e9defcee6442
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 22:07:17 +0500

    Amends controlerl function to use async await

--- app/controllers/vimeo.controller.js
+++ app/controllers/vimeo.controller.js
@@ -1,19 +1,18 @@
-const youtubeService = require("../services/vimeo.service");
+const vimeoService = require("../services/vimeo.service");
 const requestHelper = require("../utils/request.helper");
 
-exports.get = (req, res) => {
-  youtubeService.extractData(req.params.keyword)
-    .then((response) => {
-      res.send(requestHelper.getResponse(
-        true,
-        null,
-        response,
-      ));
-    })
-    .catch((error) => {
-      res.send(requestHelper.getResponse(
-        false,
-        error,
-      ));
-    });
+exports.get = async (req, res) => {
+  try {
+    const response = await vimeoService.extractData(req.params.keyword);
+    res.send(requestHelper.getResponse(
+      true,
+      null,
+      response,
+    ));
+  } catch (error) {
+    res.send(requestHelper.getResponse(
+      false,
+      error,
+    ));
+  }
 };
