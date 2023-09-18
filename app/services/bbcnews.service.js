a56eaec3c81c72b47238d021b820590eb28561ec
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 16:44:31 +0500

    Amends extract cateogry function to use async await

--- app/services/bbcnews.service.js
+++ app/services/bbcnews.service.js
@@ -2,32 +2,23 @@ const axios = require("axios");
 const parser = require("../parsers/bbc/home.parser");
 const categoryParser = require("../parsers/bbc/category.parser");
 
-exports.extractData = () => {
-  const promise = new Promise((resolve, reject) => {
-    axios(process.env.BBCNEWS_URL)
-      .then((response) => {
-        const contents = parser.parse(response.data);
-        resolve(contents);
-      })
-      .catch((error) => {
-        reject(error.message);
-      });
-  });
-
-  return promise;
+exports.extractData = async () => {
+  try {
+    const response = await axios(process.env.BBCNEWS_URL);
+    const contents = parser.parse(response.data);
+    return contents;
+  } catch (error) {
+    return error.message;
+  }
 };
 
-exports.extractCategoryData = (category) => {
-  const promise = new Promise((resolve, reject) => {
-    axios(`${process.env.BBCNEWS_URL}/news/${category}`)
-      .then((response) => {
-        const contents = categoryParser.parse(response.data);
-        resolve(contents);
-      })
-      .catch((error) => {
-        reject(error.message);
-      });
-  });
-
-  return promise;
+exports.extractCategoryData = async (category) => {
+  try {
+    const url = `${process.env.BBCNEWS_URL}/news/${category}`;
+    const response = await axios(url);
+    const contents = categoryParser.parse(response.data);
+    return contents;
+  } catch (error) {
+    return error.message;
+  }
 };
