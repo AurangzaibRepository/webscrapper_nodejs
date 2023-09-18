0d267bd8628cba2ec4a995af3c6e958924dede09
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 17:04:29 +0500

    Amends get function to use async await

--- app/controllers/dawnnews.controller.js
+++ app/controllers/dawnnews.controller.js
@@ -1,21 +1,20 @@
 const dawnnewsService = require("../services/dawnnews.service");
 const requestHelper = require("../utils/request.helper");
 
-exports.get = (req, res) => {
-  dawnnewsService.extractData()
-    .then((response) => {
-      res.send(requestHelper.getResponse(
-        true,
-        null,
-        response,
-      ));
-    })
-    .catch((error) => {
-      res.send(requestHelper.getResponse(
-        false,
-        error,
-      ));
-    });
+exports.get = async (req, res) => {
+  try {
+    const response = await dawnnewsService.extractData();
+    res.send(requestHelper.getResponse(
+      true,
+      null,
+      response,
+    ));
+  } catch (error) {
+    res.send(requestHelper.getResponse(
+      false,
+      error,
+    ));
+  }
 };
 
 exports.getByCategory = (req, res) => {
