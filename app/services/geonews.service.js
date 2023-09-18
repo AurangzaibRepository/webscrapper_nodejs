2a7da5911d88289f705310c74d148c1e6f6519a0
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 19:00:03 +0500

    Amends service function to use async await

--- app/services/geonews.service.js
+++ app/services/geonews.service.js
@@ -1,34 +1,24 @@
 const axios = require("axios");
 const parser = require("../parsers/geonews.parser");
 
-exports.extractData = () => {
-  const promise = new Promise((resolve, reject) => {
-    axios(process.env.GEONEWS_URL)
-      .then((response) => {
-        const contents = parser.parse(response.data);
-        resolve(contents);
-      })
-      .catch((error) => {
-        reject(error.message);
-      });
-  });
-
-  return promise;
+exports.extractData = async () => {
+  try {
+    const response = await axios(process.env.GEONEWS_URL);
+    const contents = parser.parse(response.data);
+    return contents;
+  } catch (error) {
+    return error.message;
+  }
 };
 
-exports.extractCategoryData = (category) => {
+exports.extractCategoryData = async (category) => {
   const url = `${process.env.GEONEWS_URL}/category/${category}`;
 
-  const promise = new Promise((resolve, reject) => {
-    axios(url)
-      .then((response) => {
-        const contents = parser.parseByCategory(response.data);
-        resolve(contents);
-      })
-      .catch((error) => {
-        reject(error.message);
-      });
-  });
-
-  return promise;
+  try {
+    const response = await axios(url);
+    const contents = parser.parseByCategory(response.data);
+    return contents;
+  } catch (error) {
+    return error.message;
+  }
 };
