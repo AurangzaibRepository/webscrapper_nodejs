b43596b4497b8815c7b5e09ba3aeafe056b75bf1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 23:56:14 +0500

    Calls puppeteer helper fucntion to initializ ean fetch hml

--- app/services/dailymotion.service.js
+++ app/services/dailymotion.service.js
@@ -3,7 +3,7 @@ const parser = require("../parsers/dailymotion.parser");
 
 exports.extractData = async () => {
   try {
-    return await puppeteer.initialize(process.env.DAILYMOTION_URL, parser.parse);
+    return await puppeteer.initialize("networkidle0", process.env.DAILYMOTION_URL, parser.parse);
   } catch (error) {
     return error.message;
   }
--- app/services/dtube.service.js
+++ app/services/dtube.service.js
@@ -1,29 +1,18 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("../utils/puppeteer.helper");
 const parser = require("../parsers/dtube/home.parser");
 const keywordParser = require("../parsers/dtube/keyword.parser");
 
 exports.extractData = async (keyword) => {
   try {
     let url = process.env.DTUBE_URL;
+    let callback = parser.parse;
 
     if (keyword != null) {
       url += `/#!/s/${keyword}`;
+      callback = keywordParser.parse;
     }
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url, {
-      waitUntil: "networkidle2",
-      timeout: 0,
-    });
-
-    const contents = (keyword == null
-      ? await parser.parse(page)
-      : await keywordParser.parse(page)
-    );
-
-    await browser.close();
-    return contents;
+    return await puppeteer.initialize("networkidle0", url, callback);
   } catch (exception) {
     return exception.message;
   }
