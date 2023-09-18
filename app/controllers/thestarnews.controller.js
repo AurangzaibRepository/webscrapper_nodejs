c75581d8e521302b96416df752b52f33dd40a472
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 21:07:45 +0500

    Amends get function to use async await

--- app/controllers/thestarnews.controller.js
+++ app/controllers/thestarnews.controller.js
@@ -1,21 +1,20 @@
 const thestarnewsService = require("../services/thestarnews.service");
 const requestHelper = require("../utils/request.helper");
 
-exports.get = (req, res) => {
-  thestarnewsService.extractData()
-    .then((response) => {
-      res.send(requestHelper.getResponse(
-        true,
-        null,
-        response,
-      ));
-    })
-    .catch((error) => {
-      res.send(requestHelper.getResponse(
-        false,
-        error,
-      ));
-    });
+exports.get = async (req, res) => {
+  try {
+    const response = await thestarnewsService.extractData();
+    res.send(requestHelper.getResponse(
+      true,
+      null,
+      response,
+    ));
+  } catch (error) {
+    res.send(requestHelper.getResponse(
+      false,
+      error,
+    ));
+  }
 };
 
 exports.getByCategory = (req, res) => {
