15558da3e94d496b58c976d5f2c43a46fb0823ef
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 20:02:55 +0500

    Amends get function to use async await

--- app/controllers/thenews.controller.js
+++ app/controllers/thenews.controller.js
@@ -1,21 +1,20 @@
 const thenewsService = require("../services/thenews.service");
 const requestHelper = require("../utils/request.helper");
 
-exports.get = (req, res) => {
-  thenewsService.extractData()
-    .then((response) => {
-      res.send(requestHelper.getResponse(
-        true,
-        null,
-        response,
-      ));
-    })
-    .catch((error) => {
-      res.send(requestHelper.getResponse(
-        true,
-        error,
-      ));
-    });
+exports.get = async (req, res) => {
+  try {
+    const response = await thenewsService.extractData();
+    res.send(requestHelper.getResponse(
+      true,
+      null,
+      response,
+    ));
+  } catch (error) {
+    res.send(requestHelper.getResponse(
+      false,
+      error,
+    ));
+  }
 };
 
 exports.getByCategory = (req, res) => {
