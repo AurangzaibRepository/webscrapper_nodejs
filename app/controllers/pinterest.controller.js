f733ca9b03ac004d9be82c68f9ad78a6c8cbabee
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 19:07:42 +0500

    Amends get function to use async await

--- app/controllers/pinterest.controller.js
+++ app/controllers/pinterest.controller.js
@@ -1,21 +1,20 @@
 const pinterestService = require("../services/pinterest.service");
 const requestHelper = require("../utils/request.helper");
 
-exports.get = (req, res) => {
-  pinterestService.extractData(req.params.keyword)
-    .then((response) => {
-      res.send(requestHelper.getResponse(
-        true,
-        null,
-        response,
-      ));
-    })
-    .catch((error) => {
-      res.send(requestHelper.getResponse(
-        false,
-        error,
-      ));
-    });
+exports.get = async (req, res) => {
+  try {
+    const response = await pinterestService.extractData();
+    res.send(requestHelper.getResponse(
+      true,
+      null,
+      response,
+    ));
+  } catch (error) {
+    res.send(requestHelper.getResponse(
+      false,
+      error,
+    ));
+  }
 };
 
 exports.getNews = (req, res) => {
