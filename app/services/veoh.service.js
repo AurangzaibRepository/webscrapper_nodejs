443478c647db162b26438aa3e637edf3e738b2f5
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 21:55:20 +0500

    Implements try catch

--- app/services/veoh.service.js
+++ app/services/veoh.service.js
@@ -3,17 +3,21 @@ const puppeteer = require("puppeteer");
 const parser = require("../parsers/veoh.parser");
 
 exports.extractData = async (keyword) => {
-  let url = process.env.VEOH_URL;
-  if (keyword != null) {
-    url += `/find/${keyword}`;
+  try {
+    let url = process.env.VEOH_URL;
+    if (keyword != null) {
+      url += `/find/${keyword}`;
+    }
+
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const contents = await parser.parse(page);
+
+    await browser.close();
+    return contents;
+  } catch (error) {
+    return error.message;
   }
-
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(url);
-
-  const contents = await parser.parse(page);
-
-  await browser.close();
-  return Promise.resolve(contents);
 };
