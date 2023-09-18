d71a37d7bee521bd668cf99c329e925ccdf43346
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 23:09:43 +0500

    Implements inittialize unction

--- app/utils/puppeteer.helper.js
+++ app/utils/puppeteer.helper.js
@@ -5,4 +5,12 @@ exports.initialize = async (
   url,
   callback,
 ) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url, options);
+
+  const contents = await callback(page);
+
+  await browser.close();
+  return contents;
 };
