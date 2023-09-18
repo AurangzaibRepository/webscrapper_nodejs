0f2ee21d3e349596c55416465e505ea19cc65f8c
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 18:03:57 +0500

    Amends function to use async await

--- app/controllers/dtube.controller.js
+++ app/controllers/dtube.controller.js
@@ -1,19 +1,18 @@
 const dtubeService = require("../services/dtube.service");
 const requestHelper = require("../utils/request.helper");
 
-exports.get = (req, res) => {
-  dtubeService.extractData(req.params.keyword)
-    .then((response) => {
-      res.send(requestHelper.getResponse(
-        true,
-        null,
-        response,
-      ));
-    })
-    .catch((error) => {
-      res.send(requestHelper.getResponse(
-        false,
-        error,
-      ));
-    });
+exports.get = async (req, res) => {
+  try {
+    const response = await dtubeService.extractData(req.params.keyword);
+    res.send(requestHelper.getResponse(
+      true,
+      null,
+      response,
+    ));
+  } catch (error) {
+    res.send(requestHelper.getResponse(
+      false,
+      error,
+    ));
+  }
 };
