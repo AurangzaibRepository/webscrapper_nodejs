8e2ad164e6575ec2fafa9b57d3661c9b1f6f4aef
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 23:48:27 +0500

    Adds network parametr

--- app/utils/puppeteer.helper.js
+++ app/utils/puppeteer.helper.js
@@ -1,13 +1,14 @@
 const puppeteer = require("puppeteer");
 
 exports.initialize = async (
+  network,
   url,
   callback,
 ) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url, {
-    waitUntil: "networkidle0",
+    waitUntil: network,
     timeout: 0,
   });
 
