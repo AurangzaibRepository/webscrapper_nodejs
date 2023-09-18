d44c5a87c43e19f7b9924b1d7cbaaefd7fe38eee
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-18 23:23:52 +0500

    Removes options paramters

--- app/utils/puppeteer.helper.js
+++ app/utils/puppeteer.helper.js
@@ -1,13 +1,15 @@
 const puppeteer = require("puppeteer");
 
 exports.initialize = async (
-  options,
   url,
   callback,
 ) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(url, options);
+  await page.goto(url, {
+    waitUntil: "networkidle0",
+    timeout: 0,
+  });
 
   const contents = await callback(page);
 
