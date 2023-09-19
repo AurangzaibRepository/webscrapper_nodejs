6f504dceff58493f71ef86af327ba5da82fb6453
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-19 22:09:37 +0500

    Amends extarctdat ti call puppeteer helper

--- app/services/youtube.service.js
+++ app/services/youtube.service.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("../utils/puppeteer.helper");
 const parser = require("../parsers/youtube/home.parser");
 const keywordParser = require("../parsers/youtube/keyword.parser");
 const channelParser = require("../parsers/youtube/channel.parser");
@@ -6,25 +6,14 @@ const channelParser = require("../parsers/youtube/channel.parser");
 exports.extractData = async (keyword) => {
   try {
     let url = process.env.YOUTUBE_URL;
+    let callback = parser.parse;
 
     if (keyword != null) {
       url += `/results?search_query=${keyword}`;
+      callback = keywordParser.parse;
     }
 
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto(url, {
-      waitUntil: "networkidle0",
-      timeout: 0,
-    });
-
-    const contents = (keyword == null
-      ? await parser.parse(page)
-      : await keywordParser.parse(page)
-    );
-
-    await browser.close();
-    return contents;
+    return await puppeteer.initialize("networkidle0", url, callback);
   } catch (exception) {
     return exception.message;
   }
