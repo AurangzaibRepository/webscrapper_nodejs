0ea2cb2d463e196fee221551ac8a9c4294b32794
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-09-19 21:50:13 +0500

    Callspuppeteer helepr fucntion

--- app/services/veoh.service.js
+++ app/services/veoh.service.js
@@ -1,22 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const puppeteer = require("puppeteer");
+const puppeteer = require("../utils/puppeteer.helper");
 const parser = require("../parsers/veoh.parser");
 
 exports.extractData = async (keyword) => {
   try {
     let url = process.env.VEOH_URL;
+
     if (keyword != null) {
       url += `/find/${keyword}`;
     }
 
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto(url);
-
-    const contents = await parser.parse(page);
-
-    await browser.close();
-    return contents;
+    return await puppeteer.initialize("networkidle0", url, parser.parse);
   } catch (error) {
     return error.message;
   }
